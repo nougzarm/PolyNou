cb224921371b12a5213a24da5f411fb4c7c54249
nougzarm <nougzarm@icloud.com>
2025-02-05 18:33:34 +0100

    supp commentaire inutile

--- main.c
+++ main.c
@@ -7,8 +7,8 @@
 */
 
 int accueil(int hauteur, int largeur){
-    attron(A_BOLD);  
-    mvprintw(0, (largeur-strlen("PolyNou"))/2, "PolyNou");  
+    attron(A_BOLD);
+    mvprintw(0, (largeur-strlen("PolyNou"))/2, "PolyNou");
     attroff(A_BOLD);
     char titre[] = "Bienvenu(e) dans PolyNou !";
     mvprintw(hauteur/2, (largeur-strlen(titre))/2, titre);
@@ -21,17 +21,17 @@ int accueil(int hauteur, int largeur){
 }
 
 int main() {
-    initscr(); 
+    initscr();
     curs_set(0);
     int hauteur, largeur;
     getmaxyx(stdscr, hauteur, largeur);
 
-    /*  Entrer sur la page d'accueil */         
+    /*  Entrer sur la page d'accueil */
     accueil(hauteur, largeur);
 
     refresh();
-    mvprintw(hauteur/2, 2, "Bravo ! vous avez réussi à entrer"); // Affiche le texte en (5,10)
-    getch();    // Attend une touche
-    endwin();   // Quitte ncurses
+    mvprintw(hauteur/2, 2, "Bravo ! vous avez réussi à entrer");
+    getch();
+    endwin();
     return 0;
 }
